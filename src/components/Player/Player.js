268803058616d79b96d5133ada8db8d0788f136c
sabovyan <abovyan.sargis90@gmail.com>
2020-08-27 01:11:06 +0400

    styled player, player, game

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,11 +3,11 @@ import styles from './Player.module.css';
 export function Player(props) {
 	return (
 		<div className={props.className}>
-			<div className={styles.player__score}>{props.score}</div>
+			<div className={props.scoreClass}>{props.score}</div>
 
 			<h2 className={styles.player__title}>{`Player ${props.id}`}</h2>
 
-			<div className={styles.player__shape}>{props.shape}</div>
+			<div className={props.shapeClass}>{props.shape}</div>
 		</div>
 	);
 }
--- src/components/Players/helper/class.helper.js
+++ src/components/Players/helper/class.helper.js
@@ -3,7 +3,9 @@ export class GamePlayer {
 		this.id = id;
 		this.className = `player player-${this.id}`;
 		this.shape = '()';
+		this.shapeClass = 'player__shape';
 		this.score = 0;
+		this.scoreClass = 'player__score';
 		this.isWinner = false;
 	}
 }
